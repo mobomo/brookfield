4885549ac1bb2e72c907ff0a4aadc13b3fd0561b
Derek Cross <derekcrosslu@gmail.com>
2022-11-15 13:32:33 -0500

    desktop version ready

--- src/frames/SecondFrame.tsx
+++ src/frames/SecondFrame.tsx
@@ -40,7 +40,7 @@ export const SecondFrame = () => {
         
           </div>
 
-          <div className='-mt-24 ml-10 '>
+          <div className='-mt-24  '>
                 <svg width="554" height="139" viewBox="0 0 554 139" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <line x1="0.5" x2="0.5" y2="100" stroke="#F5F5F5"/>
                 <line x1="553.5" y1="139" x2="553.5" y2="99" stroke="#F5F5F5"/>
